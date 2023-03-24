3b1948bdcd87cba0832c7f086795936b2e063b98
ecregger <118327201+ecregger@users.noreply.github.com>
2023-03-23 21:21:04 -0400

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -3,7 +3,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("../samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -32,7 +32,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel 
 function buildMetadata(sample) {
-  d3.json("../samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
@@ -57,7 +57,7 @@ function buildMetadata(sample) {
 // Deliverable 1: 1. Create the buildChart function.
 function buildCharts(sample) {
   // Deliverable 1: 2. Use d3.json to load the samples.json file 
-  d3.json("../samples.json").then((data) => {
+  d3.json("samples.json").then((data) => {
     console.log(data);
 
     // Deliverable 1: 3. Create a variable that holds the samples array. 
